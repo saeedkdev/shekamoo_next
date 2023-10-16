a77876b584f4fabe355aecc3d4ec2f9acbcf5be6
Saeed Karimi <saeedsektor@gmail.com>
2023-10-15 23:03:56 -0700

    working on googleId logic

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -9,6 +9,20 @@ export const config: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
     }),
   ],
+  callbacks: {
+    // get google id_token and add it to the user object with types for typescript
+    async jwt({ token, user, account }) {
+      if (account?.provider === "google" && account?.id_token) {
+        token.id_token = account.id_token;
+      }
+      return token;
+    },
+    async session({session, token}) {
+      session.googleId = token.id_token;
+      return session;
+    }
+  },
+
 };
 
 const handler = NextAuth(config)
--- components/site-header.tsx
+++ components/site-header.tsx
@@ -12,6 +12,8 @@ import { signIn, signOut, useSession } from "next-auth/react"
 
 export function SiteHeader(): JSX.Element {
   const session = useSession()
+  const googleId = session.data;
+  console.log(googleId)
   return (
     <header className="bg-background sticky top-0 z-40 w-full border-b">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
